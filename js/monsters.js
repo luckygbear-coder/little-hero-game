80c0cca802ce528118ffe732024944e9148b2794
luckygbear-coder <luckygbear@gmail.com>
2025-12-02 19:15:48 +0800

    创建 monsters.js

---
+++ js/monsters.js
@@ -0,0 +1,42 @@
+// ===============================
+// 🧟 一般魔物設定 monsters.js
+// ===============================
+
+export const monstersConfig = {
+  "哭哭史萊姆": {
+    talent: "rock",        // 天賦拳
+    forbidden: "paper"     // 不能出的拳
+  },
+  "暴躁火球": {
+    talent: "rock",
+    forbidden: "paper"
+  },
+  "壞心情南瓜": {
+    talent: "rock",
+    forbidden: "paper"
+  },
+  "哭哭菇菇": {
+    talent: "paper",
+    forbidden: "scissors"
+  },
+  "悲傷骷髏": {
+    talent: "rock",
+    forbidden: "paper"
+  },
+  "黏黏史萊姆": {
+    talent: "rock",
+    forbidden: "paper"
+  },
+  "兇暴狼人": {
+    talent: "rock",
+    forbidden: "paper"
+  },
+  "小惡魔": {
+    talent: "rock",
+    forbidden: "scissors"
+  },
+  "憂鬱幽靈": {
+    talent: "paper",
+    forbidden: "rock"
+  }
+};
